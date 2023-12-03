0316a39a726d3b974d664afa68f951dd1c0766de
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-12-03 17:57:06 +0000

    Add day 1 part 2 solution

--- day-1/trebuchetPart2.py
+++ day-1/trebuchetPart2.py
@@ -1,10 +1,64 @@
 def parseFile():
-  with open("day-1\part2Example.txt") as inputFile:
+  with open("day-1\input.txt") as inputFile:
     lines = []
     data = inputFile.readlines()
     for i in range(len(data)):
       lines.append(data[i].strip())
   return lines
 
+def sumCalibrationValues(document):
+  numbers = {
+    'one':'1',
+    'two':'2',
+    'three':'3',
+    'four':'4',
+    'five':'5',
+    'six':'6',
+    'seven':'7',
+    'eight':'8',
+    'nine':'9'
+  }
 
-print(parseFile())
+  sum = 0
+
+  for line in document:
+    calibrationValue = ''
+    for startIndex in range(len(line)):
+      numFound = False
+
+      if line[startIndex].isdigit():
+        calibrationValue += line[startIndex]
+        numFound = True
+        break
+
+      for endIndex in range(startIndex, len(line)+1):
+        if line[startIndex:endIndex] in numbers:
+          calibrationValue += numbers[line[startIndex:endIndex]]
+          numFound = True
+
+      if numFound:
+        break
+
+    for endIndex in range(len(line), -1, -1):
+      numFound = False
+
+      if line[endIndex-1].isdigit():
+        calibrationValue += line[endIndex-1]
+        numFound = True
+        break
+
+      for startIndex in range(endIndex, -1, -1):
+        if line[startIndex:endIndex] in numbers:
+          calibrationValue += numbers[line[startIndex:endIndex]]
+          numFound = True
+
+      if numFound:
+        break
+    
+    print(int(calibrationValue))
+    sum += int(calibrationValue)
+          
+  return sum
+    
+
+print(sumCalibrationValues(parseFile()))
