875f21d0fd6f15976f81095d93da3b5977a291af
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-12-04 18:23:00 +0000

    Add day 4 part 1 solution

--- day-4/scratchcardsPart1.py
+++ day-4/scratchcardsPart1.py
@@ -5,3 +5,26 @@ def parseFile():
     for i in range(len(data)):
       lines.append(data[i].strip())
   return lines
+
+def calculatePoints(cardsData):
+  winningNumbers = []
+  acquiredNumbers = []
+  totalPoints = 0
+
+  for card in cardsData:
+    points = 0
+    winningNumbers = list(map(int, card.split(":")[1].split("|")[0].split()))
+    acquiredNumbers = list(map(int, card.split(":")[1].split("|")[1].split()))
+    
+    for number in acquiredNumbers:
+      if number in winningNumbers:
+        if (points != 0):
+          points *= 2
+        else:
+          points += 1
+
+    totalPoints += points
+
+  return totalPoints
+
+print(calculatePoints(parseFile()))
