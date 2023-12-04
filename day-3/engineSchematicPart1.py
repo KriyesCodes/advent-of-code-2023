bb94c2743f687fef6b5f147b2ad246d79b9fd8f5
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-12-04 17:39:41 +0000

    Fix parseInput function

--- day-3/engineSchematicPart1.py
+++ day-3/engineSchematicPart1.py
@@ -11,8 +11,10 @@ def sumSchematicPartNumbers(schematic):
   sum = 0
   currentNumber = ''
   currentNumberIsPart = False
+  lineSums = []
 
   for y in range(len(schematic)):
+    lineSum = 0
     for x in range(len(schematic[y])):
       currentChar = schematic[y][x]
 
@@ -27,17 +29,20 @@ def sumSchematicPartNumbers(schematic):
           if ord(nextChar) < 48 or ord(nextChar) > 57:
             if currentNumberIsPart:
               sum += int(currentNumber)
+              lineSum += int(currentNumber)
               # print("x+1 less than bounds: " + currentNumber) #debug
             currentNumber = ''
             currentNumberIsPart = False
         else:
           if currentNumberIsPart:
               sum += int(currentNumber)
+              lineSum += int(currentNumber)
               # print("x+1 more than bounds: " + currentNumber) # debug
           currentNumber = ''
           currentNumberIsPart = False
+    lineSums.append(lineSum)
 
-  return sum
+  return lineSums
 
 def checkForGearSymbolAround(schematic, digitX, digitY):
   if not(digitY - 1 < 0):
@@ -79,7 +84,20 @@ def checkForGearSymbolAround(schematic, digitX, digitY):
       return True
     
   return False
-  
 
-print("sample 4:",sumSchematicPartNumbers(parseInput("day-3\sample4.txt")))
-print("sample 5:",sumSchematicPartNumbers(parseInput("day-3\sample5.txt")))
+
+def parseSumsInput(PATH):
+  with open(PATH) as inputFile:
+    data = inputFile.readlines()
+    schematic = []
+    for i in range(len(data)):
+      schematic.append(data[i].strip())
+
+    return schematic
+  
+theirLineData = parseSumsInput('day-3\lineSum.txt')
+myLineData = sumSchematicPartNumbers(parseInput("day-3\sample4.txt"))
+
+print("{:^15}{:^15}{:^15}{:^15}".format("line num","your line sums", "my line sums", "difference"))
+for i in range (len(theirLineData)):
+  print("{:^15}{:^15}{:^15}{:^15}".format(i+1, theirLineData[i], myLineData[i], abs(int(theirLineData[i]) - int(myLineData[i]))))
