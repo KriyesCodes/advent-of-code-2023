8efb814342024243d91be1a8e5081db62059fa30
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-12-04 18:58:42 +0000

    Add day 4 part 2 solution

---
+++ day-4/scratchcardsPart2.py
@@ -0,0 +1,33 @@
+def parseFile():
+  with open("day-4\input.txt") as inputFile:
+    lines = []
+    data = inputFile.readlines()
+    for i in range(len(data)):
+      lines.append(data[i].strip())
+  return lines
+
+def calculatePoints(cardsData):
+  cardInstances = {}
+  for card in cardsData:
+    cardInstancesKey = int(card.split(":")[0].split()[1])
+    cardInstances[cardInstancesKey] = 1
+
+  for card in cardsData:
+    cardKey = int(card.split(":")[0].split()[1])
+    winningNumbers = list(map(int, card.split(":")[1].split("|")[0].split()))
+    acquiredNumbers = list(map(int, card.split(":")[1].split("|")[1].split()))
+
+    matches = 0
+    
+    for number in acquiredNumbers:
+      if number in winningNumbers:
+        matches += 1
+
+    for i in range(cardInstances[cardKey]):
+      for i in range(cardKey+1, cardKey + matches + 1):
+          cardInstances[i] += 1
+    
+
+  return sum(cardInstances.values())
+
+print(calculatePoints(parseFile()))
